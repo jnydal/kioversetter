312baf309b2aac375a33ffd50ae93bda1dfba257
Thor Nydal <jnydal@gmail.com>
2025-03-25 13:57:53 +0100

    fixed workflow..

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "/aitranscribe/",
+  base: "/aitranscribe/",
+    build: {
+      outDir: 'dist',  // This should match the output folder
+    },
 })
